a30418e590ba35982226a9a2fea900661ac103c8
sergiobandera10@gmail.com <sergiobandera10@gmail.com>
2020-07-02 23:10:08 -0500

    nivelar

---
+++ js/myscript.js
@@ -0,0 +1,45 @@
+//Ejercicio 1
+function TipoMotor ( )  {
+    var  n1 = prompt ( "Indique por favor el tipo de Motor, los valores posibles son 1, 2, 3, 4" ) ;
+    interruptor  (n1) 
+      case  0 :
+        document.write ( "No hay establecido un valor definido para el tipo debomba" ) ;
+        break ;    
+    case  1 :
+        document.write ( "La bomba es una bomba de agua" ) ;
+        brake ;
+    case  2 :
+        document.write  ( "La bomba es una bomba de gasolina" ) ;
+        brake ;
+    case  3 :
+        document.write ( "La bomba es una bomba de hormigón" ) ;
+        brake ;
+    case  4 :
+        document.write( "La bomba es una bomba de pasta alimenticia" ) ;
+        break ;
+     default :
+        document.write ( "No existe un valor válido para tipo de bomba" ) ;
+    };
+
+
+//EJERCICIO 2
+
+function   ObtenerImporteConImpuestos ( )  {
+    var  valorsinimpuestos = prompt ( "Indique por favor el valor sin impuestos" ) ;
+    var  tipodeproducto = prompt ( "Indique por favor el tipo de producto" ) ;
+    var  valor ;
+    
+    
+    case  1 
+        value = ( ( valorsinimpuestos * 21 ) / 100 ) + ("valorsinimpuestos") ;
+        alert ( "valor" ) ;
+        break ;    
+    case 2 
+        value = ( ( valorsinimpuestos * 10 ) / 100 ) + ("valorsinimpuestos") ;
+        alert ( "valor" ) ;
+        break ;
+    case  3 
+        value = ( ( valorsinimpuestos * 5 ) / 100 ) + ("valorsinimpuestos") ;
+        alert ( "valor" ) ;
+        break ;
+}
